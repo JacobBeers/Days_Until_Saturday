a9f573a8c79d42860a46bd0cef495b85b935713b
JacobBeers <aguynamedjacob@gmail.com>
2022-07-13 09:25:07 -0500

    day script

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+switch (new Date().getDay()) {
+    case 1:
+        console.log(`Today is Monday, 5 days until Saturday!Today is Monday, 5 days until Saturday!`);
+        break;
+    case 2:
+        console.log(`Today is Monday, 4 days until Saturday!`);
+        break;
+    case 3: 
+        console.log(`Today is Wednesday, only 3 days until Saturday.`);
+        break;
+    case 4:
+        console.log(`Today is Thursday, only 3 days until Saturday!`);
+        break;
+    case 5:
+        console.log(`Happy Friday, tomorrow is the weekend!`);
+        break;
+    case 6:
+      text = console.log(`It is the Weekend!!`);
+      break;
+    default:
+      text = "Looking forward to next Weekend";
+  }
+
+  
+
 
